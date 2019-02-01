a4160ce15931c8a50d654dd42fd414506aaed924
Joy <stepjoyous@gmail.com>
2019-01-31 17:42:34 -0800

    Attempting to ping brewery API to bring back brewery list.

--- client/src/components/BrewIndex/index.js
+++ client/src/components/BrewIndex/index.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-export default class BrewIndex extends Component {
+class BrewIndex extends Component {
+    state = {
+        brewery: []
+    }
+    
+    componentDidMount () {
+        let { brewery } = this.state;
+        debugger
+        axios.get(`https://api.brewerydb.com/v2/locations/?key=e64cdf34c138b3ad00ce4a5de938d8f1&id=doVlXA`)
+            .then(res  => {
+                console.log( res )
+                debugger
+                this.setState({ brewery: res.data })
+            })
+    }
 
     render() {
+        let { brewery, index } = this.state;
+        debugger
         return(
             <div className="container">
                 <h1>BREWERY INDEX</h1>
@@ -10,7 +27,7 @@ export default class BrewIndex extends Component {
                     <h1>Insert map here</h1>
                 </div>
                 <div className="container">
-                    <div className="brewery">1 brewery
+                    <div className="brewery"> {brewery.name}
                     </div>
                     <div className="brewery">2 brewery
                     </div>
@@ -21,3 +38,4 @@ export default class BrewIndex extends Component {
         )
     }
 }
+export default BrewIndex;
