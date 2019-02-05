9bd95f43c9765a85d3d11e6489949b2c7674a6db
Joy <stepjoyous@gmail.com>
2019-02-05 10:27:10 -0800

    Fixing merge issues

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -16,24 +16,6 @@ class Profile extends Component {
         
         // Ping Foursquare API to retrieve allBreweries info
         await axios.get('/test')
-<<<<<<< HEAD
-        .then(res => {
-            // debugger
-            this.setState({ allBreweries: res.data.response.venues })
-        }).catch(err => {
-            debugger
-        })
-
-        await axios.get(`/api/users/${currentUser._id}`)
-        .then(({ data })  => {
-            this.setState({ user: data.payload, favorites: data.payload.favorites, loading: false})
-            // debugger
-        }).catch(err => {
-            debugger
-        })
-
-        // debugger
-=======
             .then(res => {
                 this.setState({ allBreweries: res.data.response.venues })
             }).catch(err => {
@@ -47,46 +29,10 @@ class Profile extends Component {
             }).catch(err => {
                 debugger
             })
->>>>>>> 4da88ae3151f9ed924f4db32ef4b08bbb0f7b43a
     }
 
     render() {
         let { user, loading, favorites, allBreweries } = this.state;
-<<<<<<< HEAD
-        // favorites = "all of the users fav breweries by their brewId"
-        // allBreweries = "all of the breweries in the API"
-        // debugger
-        // iterate through favorites
-        var filteredBrewery = [];
-        for (var i = 0; i < favorites.length; i++) {
-            var id = favorites[i];
-            // for each id in favorites, find the object in allBreweries that has a matching id
-            allBreweries.filter((element) => {
-                if (element.id === id) {
-                    // debugger
-                    // save that object in a variable array to store them for use in the render
-                    filteredBrewery.push(element)
-                }
-            })
-            // debugger
-        }
-        
-        debugger
-        // iterate through filteredBrewery array and display each 
-        if (loading) return <div></div>
-        return (
-            <div>
-                <h1> { user.name } Profile </h1>
-                <h3> { user.email } </h3>
-                <Link to="/edit">Edit Profile </Link>
-                <div className="row">
-                    <ul>
-                    {filteredBrewery.map((eachBrewery) => {
-                        return <li>{eachBrewery.name}</li>
-                    })}
-                    </ul>
-                </div>
-=======
 
         // Loop through allBreweries to search and retrieve info using brewId in user's favorites
         var filteredBrewery = [];
@@ -122,7 +68,6 @@ class Profile extends Component {
                         </div>
                     })} 
                 </ul>
->>>>>>> 4da88ae3151f9ed924f4db32ef4b08bbb0f7b43a
             </div>
         </div>
         )
