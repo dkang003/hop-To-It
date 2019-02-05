b5964cbf0d742c6a5ea3f6c51ea821d41b180659
Daniel Kang <d.kang106@gmail.com>
2019-02-04 23:57:57 -0800

    breweries list on profile page

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -18,31 +18,21 @@ class Profile extends Component {
 
         await axios.get('/test')
         .then(res => {
-            debugger
+            // debugger
             this.setState({ allBreweries: res.data.response.venues })
         }).catch(err => {
             debugger
         })
 
-        // axios('/test')
-        // .then(res => { 
-            
-        //     this.setState({ breweries: res.data.response.venues })
-        // })
-        // .catch(err => {
-        //     debugger
-        // })
-        // this.setState({ currentUser: this.props.currentUser})
-
         await axios.get(`/api/users/${currentUser._id}`)
         .then(({ data })  => {
             this.setState({ user: data.payload, favorites: data.payload.favorites, loading: false})
-            debugger
+            // debugger
         }).catch(err => {
             debugger
         })
 
-        debugger
+        // debugger
     }
 
 
@@ -50,8 +40,26 @@ class Profile extends Component {
 
     render () {
         let { user, loading, favorites, allBreweries } = this.state;
+        // favorites = "all of the users fav breweries by their brewId"
+        // allBreweries = "all of the breweries in the API"
+        // debugger
+        // iterate through favorites
+        var filteredBrewery = [];
+        for (var i = 0; i < favorites.length; i++) {
+            var id = favorites[i];
+            // for each id in favorites, find the object in allBreweries that has a matching id
+            allBreweries.filter((element) => {
+                if (element.id === id) {
+                    // debugger
+                    // save that object in a variable array to store them for use in the render
+                    filteredBrewery.push(element)
+                }
+            })
+            // debugger
+        }
+        
         debugger
-        // user.favorites is the array of users favorite breweries by mongo id
+        // iterate through filteredBrewery array and display each 
         if (loading) return <div></div>
         return (
             <div>
@@ -60,7 +68,9 @@ class Profile extends Component {
                 <Link to="/edit">Edit Profile </Link>
                 <div className="row">
                     <ul>
-                        <li>Brewery list starts here</li>
+                    {filteredBrewery.map((eachBrewery) => {
+                        return <li>{eachBrewery.name}</li>
+                    })}
                     </ul>
                 </div>
             </div>
